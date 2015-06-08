35da14b4bdb4f728a7381736cb6c0ab683d5f1b5
LarsdeGraaff <lars.dg@gmail.com>
2015-06-08 14:10:19 +0200

    added calculator class + test one and two numbers(2)

--- src/main/java/com/realdolmen/Calculator.java
+++ src/main/java/com/realdolmen/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 
     public int add(String s) {
         int a=0;
-        for (int i=1;i<s.length();i++) {
+        for (int i=0;i<s.length();i=i+2) {
             a = a + Integer.parseInt(s.valueOf(s.charAt(0)));
 
         }
